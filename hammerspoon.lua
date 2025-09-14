3edd724ad7efc2d64da9597926a9bd0be935dd41
TheDro <andrewsc32@protonmail.com>
2025-09-14 09:48:00 -0400

    Added hammerspoon script

---
+++ hammerspoon.lua
@@ -0,0 +1,19 @@
+hs.hotkey.bind({"cmd", "shift"}, "1", function()
+  hs.application.launchOrFocus("RubyMine")
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "2", function()
+  hs.application.launchOrFocus("Cursor")
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "3", function()
+  hs.application.launchOrFocus("Terminal")
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "4", function()
+  hs.application.launchOrFocus("Google Chrome")
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "5", function()
+  hs.application.launchOrFocus("Slack")
+end)
